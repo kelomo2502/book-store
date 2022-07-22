e3b081770645f3def86198453b535c02e969f94a
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-22 21:37:39 +0100

    Add Book component

---
+++ src/Components/Books.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import BookList from './BookList';
+import FormInput from './FormInput';
+
+class Book extends React.Component {
+        state = {
+          books: [
+            {
+              id: 1,
+              title: 'Things Fall Apart',
+              author: 'Chinua Achebe',
+            },
+
+          ],
+
+        }
+
+        render() {
+          return (
+            <div>
+              <BookList books={this.state.books} />
+              <div>
+                <FormInput />
+              </div>
+            </div>
+
+          );
+        }
+}
+
+export default Book;
