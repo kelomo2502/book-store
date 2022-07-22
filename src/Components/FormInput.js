3f24de5b8172909d87dbc6e2eaefda66fe341e86
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-22 21:39:17 +0100

    Add formInput component

---
+++ src/Components/FormInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class FormInput extends React.Component {
+  render() {
+    return (
+      <div className='title-author'>
+        <h3>ADD NEW BOOK</h3>
+        <input type="text" placeholder="Enter Title" />
+        <input type="text" placeholder="Enter Author" />
+        <button>Add Book</button>
+      </div>
+
+    );
+  }
+}
+
+export default FormInput;
