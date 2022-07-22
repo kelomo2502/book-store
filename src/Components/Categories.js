23a1ea7ddd158bb809518b11ecf84a6d0ee83f2a
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-22 21:38:27 +0100

    Add catgory component

---
+++ src/Components/Categories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Categories extends React.Component {
+  render() {
+    return (
+
+      <div>
+        <button>Check Status</button>
+
+      </div>
+    );
+  }
+}
+
+export default Categories;
