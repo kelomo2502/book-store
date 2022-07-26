d397711e43d42176eca31e64947c7364735e8251
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-26 14:05:49 +0100

    modify home.js

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import BookList from './BookList';
+import Form from './Form';
+
+const BooksContainer = () => (
+  <section className="booklist">
+    <BookList />
+    <Form />
+  </section>
+);
+
+export default BooksContainer;
