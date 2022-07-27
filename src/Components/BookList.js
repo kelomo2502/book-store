99f39324b85b628a851d75b7b9271b3743fe123c
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-27 11:34:31 +0100

    import Import use selector to booklist

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,14 +1,19 @@
 import React from 'react'
-
+import { useSelector, shallowEqual } from 'react-redux';
 import Book from './Book';
 
 const BookList = () => {
-  const books = [{
-    id: 1, title: 'title', author: 'author', genre: 'genre',
-  }];
+  const books = useSelector((state) => state.books, shallowEqual);
 
   return books.map((book) => (
-    <Book genre={book.genre} title={book.title} author={book.author} key={book.id} />
+    <Book
+      genre={book.genre}
+      title={book.title}
+      author={book.author}
+      key={book.id}
+      completed={book.completed}
+      chapter={book.chapter}
+    />
   ));
 };
 
