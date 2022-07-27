28fe6f395ff5ef968937daefc2d229e96dd58e21
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-27 11:30:07 +0100

    Add initialState to configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,15 @@ import { combineReducers, createStore } from 'redux';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
+const initialState = {
+  books: [{
+    id: 1,
+    title: 'Things fall apart ',
+    author: 'Chinua Achebe',
+  }],
+};
+
 const rootReducer = combineReducers({ books: booksReducer, categories: categoriesReducer });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, initialState);
 
 export default store;
