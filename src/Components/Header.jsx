710ae29361249ee3bfd8c15d227cc8ef1cc24495
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-29 17:50:01 +0100

    Add padding and margin based on zeplin design

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Bookstore</h1>
-    <nav>
-      <Link to="/" className="nav-link">BOOKS</Link>
-      <Link to="/categories" className="nav-link">CATEGORIES</Link>
-    </nav>
+    <div className="logo-nav">
+      <h1>Bookstore</h1>
+      <nav>
+        <Link to="/" className="nav-link">
+          BOOKS
+        </Link>
+        <Link to="/categories" className="nav-link categories">
+          CATEGORIES
+        </Link>
+      </nav>
+    </div>
+
     <i className="fas fa-user-alt"></i>
   </header>
 );
